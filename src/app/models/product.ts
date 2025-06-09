f5ad03133ddbf3378619f2b0f3e36955373f8749
Rocco <13131155@me.mcu.edu.tw>
2025-06-09 14:39:48 +0800

    feat(product): 建立產品資料模型

---
+++ src/app/models/product.ts
@@ -0,0 +1,22 @@
+export class Product {
+  constructor(iniData?: Partial<Product>) {
+    if (!iniData) return;
+    Object.assign(this, iniData);
+  }
+
+  id!: number;
+
+  name!: string;
+
+  company!: string;
+
+  authors!: string;
+
+  price!: number;
+
+  isShow!: boolean;
+
+  photoUrl!: string;
+
+  createDate!: Date;
+}
