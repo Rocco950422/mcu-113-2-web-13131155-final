9988378fc39cf5c238f529b4658d688fe09f0a3f
Rocco <13131155@me.mcu.edu.tw>
2025-06-13 00:37:02 +0800

    feat(router): 切換產品清單與購物車頁面實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
